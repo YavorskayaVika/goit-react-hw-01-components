fc0c1cb10e5c5a9773b8125c6b209ea9c5d82a41
YavorskayaVika <135236285+YavorskayaVika@users.noreply.github.com>
2023-09-07 20:08:07 +0300

    profile

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,6 +1,31 @@
-import React from "react";
-  
+import React from 'react';
 
-export const ProfileCard = ({ username,tag,location,avatar,stats }) =>{
-  
-}
+export const ProfileCard = ({ username, tag, location, avatar, stats }) => {
+  return (
+    <div>
+      <div className="profile">
+        <div className="description">
+          <img src={avatar} alt="User avatar" />
+          <p className="name"> {username}</p>
+          <p className="tag">@{tag}</p>
+          <p className="location">{location}</p>
+        </div>
+
+        <ul className="stats">
+          <li className="stats-data">
+            <span className="label">Followers</span>
+            <span className="quantity">{stats.followers}</span>
+          </li>
+          <li className="stats-data">
+            <span className="label">Views</span>
+            <span className="quantity">{stats.views}</span>
+          </li>
+          <li className="stats-data">
+            <span className="label">Likes</span>
+            <span className="quantity">{stats.likes}</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
